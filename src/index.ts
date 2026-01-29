b490565bf717e156bc0cc99ca85602463653d146
iiivanpopov <iiivanpopov999@gmail.com>
2026-01-29 20:38:32 +0000

    🎉 remove redundant shit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export { stylelint } from './stylelint'
+import type { Config as StylelintConfig } from 'stylelint'
+
+export function stylelint(config: StylelintConfig = {}): StylelintConfig {
+  return {
+    extends: [
+      'stylelint-config-standard',
+      'stylelint-config-recommended',
+      'stylelint-config-recess-order',
+      ...(Array.isArray(config.extends) ? config.extends : []),
+    ],
+    rules: {
+      'at-rule-no-unknown': null,
+      'import-notation': null,
+      'property-no-unknown': null,
+      ...config.rules,
+    },
+  }
+}
--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function mergeObjects(source: Record<string, any>, target: Record<string, any>): object {
-  for (const [key, value] of Object.entries(target)) {
-    if (Array.isArray(value)) source[key] = [...(source[key] || []), ...value]
-    else if (value !== null && typeof value === 'object')
-      source[key] = mergeObjects(source[key] || {}, value)
-    else source[key] = value
-  }
-  return source
-}
